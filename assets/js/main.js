8832caacb3ec70c50ad64bccd09fa45d83b5afe9
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-11-29 12:29:20 -0300

    tentativa 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
 const cart = [...document.querySelectorAll(".cart")]
 let cartNum = document.querySelector(".cartNum")
+const teste = document.getElementById("teste")
 let count = 0
+
 cart.map((el) => {
     el.addEventListener("click", (evt) => {
-        const divFather = evt.target.parentElement.parentElement;
+        const elementoId = el.id
+        teste.append(el.id)
 
-        
         //QUANTIDADE DE VEZES QUE O ELEMENTO É CLICADO//
         cartNum.textContent = count; 
         count++;
