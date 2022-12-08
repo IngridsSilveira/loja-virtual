dc16785ede6217158d70a4d3b528f772da144a07
IngridsSilveira <ingridsouzaok@gmail.com>
2022-12-08 12:19:55 -0300

    readme add

---
+++ README.md
@@ -0,0 +1,26 @@
+# E-commerce Fruit-Frutas
+ E-commerce fictício sobre uma loja de frutas, onde você pode realizar a compra das frutas exibidas no catálogo.
+
+ ° Adicionar no carrinho; <br>
+ ° Remover do carrinho;
+
+Feito com HTML, SCSS, Bootstrap e Javascript
+
+## Table of contents
+
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Author](#author)
+
+### Screenshot
+
+![](assets/images/Screenshot%202022-12-08%20at%2012-15-53%20Fruit%20-%20Frutas.png)
+
+### Links
+
+- Solution URL: [Solution here](https://ingridssilveira.github.io/loja-virtual/)
+
+## Author
+
+- Website - [Ingrid Souza](https://ingridssilveira.github.io/IngridSouza)
+- GitHub - [@IngridsSilveira](https://github.com/IngridsSilveira)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,56 @@
-const cart = [...document.querySelectorAll(".cart")]
-let cartNum = document.querySelector(".cartNum")
-const caixaDiv = document.getElementById("caixaDiv")
-const total = document.getElementById("total")
+const cart = [...document.querySelectorAll(".cart")];
+let cartNum = document.querySelector(".cartNum");
+const caixaDiv = document.getElementById("caixaDiv");
+const total = document.getElementById("total");
+let valor = document.getElementById("valor").innerHTML;
 
 const criar = (elemento) => {
-    //CRIA UM ELEMENTO BASEADO NO CLICK DO CARRINHO
-    const novoElemento = document.createElement("div");
-    novoElemento.setAttribute("class", "div-itens");
-    novoElemento.innerHTML = elemento.id;
+  //CRIA UM ELEMENTO BASEADO NO CLICK DO CARRINHO
+  const novoElemento = document.createElement("div");
+  novoElemento.setAttribute("class", "div-itens");
+  novoElemento.innerHTML = elemento.id;
 
-    //CRIA A DIV COM O A LIXEIRA
-    const lixeira = document.createElement("div")
-    lixeira.setAttribute("id", "lixeira")
-    lixeira.innerHTML = `<i class="bi bi-trash3-fill"></i>`
+  //CRIA A DIV COM O A LIXEIRA
+  const lixeira = document.createElement("div");
+  lixeira.setAttribute("id", "lixeira");
+  lixeira.innerHTML = `<i class="bi bi-trash3-fill"></i>`;
 
-    //COLOCA OS ELEMENTOS DENTRO DAS DIVS
-    novoElemento.appendChild(lixeira)
-    caixaDiv.appendChild(novoElemento)
+  //COLOCA OS ELEMENTOS DENTRO DAS DIVS
+  novoElemento.appendChild(lixeira);
+  caixaDiv.appendChild(novoElemento);
 
-    //REMOVENDO ELEMENTOS
-    const lixeiraBtn = [...document.querySelectorAll("#lixeira")]
-    lixeiraBtn.map((el) => {
-        el.addEventListener("click", (evt) => {
-            const alvo = evt.target
-            const parenteAlvo = alvo.parentElement.parentElement
-            parenteAlvo.remove()
-           
-                if(caixaDiv.children.length >= 0){
-                    cartNum.textContent = caixaDiv.children.length
-                } 
-                else if(caixaDiv.children.length == 0) {
-                    cartNum.textContent = caixaDiv.children.length
-                }
-        })
-    })
-}
+  //REMOVENDO ELEMENTOS
+  const lixeiraBtn = [...document.querySelectorAll("#lixeira")];
+  lixeiraBtn.map((el) => {
+    el.addEventListener("click", (evt) => {
+      const alvo = evt.target;
+      const parenteAlvo = alvo.parentElement.parentElement;
+      parenteAlvo.remove();
+
+      if (caixaDiv.children.length >= 0) {
+        cartNum.textContent = caixaDiv.children.length;
+      } else if (caixaDiv.children.length == 0) {
+        cartNum.textContent = caixaDiv.children.length;
+      }
+    });
+  });
+};
 
 cart.map((el) => {
-    el.addEventListener("click", (evt) => {
-        //Criando o elemento
-        criar(el)
+  el.addEventListener("click", (evt) => {
+    //Criando o elemento
+    criar(el);
 
-        //QUANTIDADE DE VEZES QUE O ELEMENTO É CLICADO//
-        if(caixaDiv.children.length >= 0){
-            cartNum.textContent = caixaDiv.children.length
-        } 
-        
-    })
-})
+    console.log(evt.target)
+
+    const valorNumerico = valor.replace(/[^\d,]/g, "");
+    const teste = parseFloat(valorNumerico) + parseFloat(valorNumerico);
+
+    total.innerHTML = `R$ ${teste}`;
+
+    //QUANTIDADE DE VEZES QUE O ELEMENTO É CLICADO//
+    if (caixaDiv.children.length >= 0) {
+      cartNum.textContent = caixaDiv.children.length;
+    }
+  });
+});
